d06ea16cbac9da19dc6d48a3d3adafc8e2f69d03
Владислав Медведевский <miedviedievskii@mail.ru>
2025-01-16 00:49:23 +0500

    9. Конфигурация базового URL API через переменную окружения

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const ROOT_API_URL: string = 'http://localhost';
+export const ROOT_API_URL: string = import.meta.env.VITE_API_URL || 'http://localhost';
 // API_URL: import.meta.env.VITE_API_URL
